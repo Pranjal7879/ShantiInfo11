19484a973362ad019f67d70429d7ad41a65add19
pranjal shukla <138642719+Pranjal7879@users.noreply.github.com>
2024-03-17 21:07:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+
 
 dotenv.config();
 
@@ -22,12 +22,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
